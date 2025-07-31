a76693a565009e0a58fba684798be10d6da2b458
ADMIN <ADMIN@DESKTOP-GM8H725>
2025-07-31 16:13:57 +0530

    Git Project

---
+++ src/main/java/com/brsons/BrsonsApplication.java
@@ -0,0 +1,13 @@
+package com.brsons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrsonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrsonsApplication.class, args);
+	}
+
+}
